70a49808e1d4cecd86f07954d080cec061a0aa4f
Alexis Flodev <floresfernandezalexishr@gmail.com>
2025-05-08 21:26:51 -0600

    Global state
    eplace all occurrences of orig_termios with E.orig_termios

--- kilo.c
+++ kilo.c
@@ -13,8 +13,11 @@
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+  struct termios orig_termios;
+};
 
+struct editorConfig E;
 /*** terminal ***/
 
 void die(const char *s) {
@@ -25,15 +28,15 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetttr");
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcsetattr");
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcsetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag &= ~(CS8);
