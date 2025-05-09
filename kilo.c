6a8c6aef2262ed088f3b95d032779c23c306959e
Alexis Flodev <floresfernandezalexishr@gmail.com>
2025-05-08 20:52:47 -0600

    clear the screen on exit
    add two exits on die() and swich on editorProcessKeypress()

--- kilo.c
+++ kilo.c
@@ -18,6 +18,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
@@ -65,8 +67,10 @@ void editorProcessKeyPress() {
 
     switch (c) {
         case CTRL_KEY('q'):
-            exit(0);
-            break;
+          write(STDOUT_FILENO, "\x1b[2J", 4);
+          write(STDOUT_FILENO, "\x1b[H", 3);
+          exit(0);
+          break;
     }
 }
 
