b1787711d76a504cb49cd9f6e7f07afed3d277e5
Alexis Flodev <floresfernandezalexishr@gmail.com>
2025-05-14 07:38:39 -0600

    Clear lines one at time
    remove <esc>[2J, and instead put a <esc>K

--- kilo.c
+++ kilo.c
@@ -121,6 +121,7 @@ void editorDrawRows(struc abuf *ab) {
   for (y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
 
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows -1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -131,7 +132,6 @@ void editorRefresScreen() {
   struc abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6);
-  abAppend(&ab, "\x1b[2J", 4);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
